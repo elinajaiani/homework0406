45fc539dcc30c2627259305a84d85baf32deccac
elina jaiani <104080573+elinajaiani@users.noreply.github.com>
2022-06-04 11:45:58 +0400

    fruits

--- homework0406.js
+++ homework0406.js
@@ -16,3 +16,35 @@ else if (month < 8) {
 else { (month < 12) 
     welcome = "უფრო მაგრად წვიმს";
 }
+
+////////
+
+
+var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" ]
+var month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+
+
+if (day > 4 , month <4){
+welcome = "მივდივართ სათხილამუროდ"
+}
+
+else if (month <9) {
+    welcome = "მივდივართ ზღვაზე";
+}
+
+else {
+    welcome = "ვმუშაობთ"
+}
+
+////////
+
+
+var fruit = ["ვაშლი", "მსხალი", "ქლიავი"]
+fruit.join ("ვაშლი", "მსხალი", "ქლიავი")
+
+var fruit = ["ვაშლი", "მსხალი", "ქლიავი"]
+fruit.splice (0,2)
+
+
+
